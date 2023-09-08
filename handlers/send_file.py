91e8eb16a122293c7ad960696029042007744661
MrAbhii2k3 <128618095+MrAbhii2k3@users.noreply.github.com>
2023-09-08 15:39:05 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -1,6 +1,9 @@
 # Updated By @MrAbhi2k3
 
 import asyncio
+import requests
+import string
+import random
 from configs import Config
 from pyrogram import Client
 from pyrogram.types import Message
@@ -10,7 +13,8 @@ from handlers.helpers import str_to_b64
 async def reply_forward(message: Message, file_id: int):
     try:
         await message.reply_text(
-            f"Files will be deleted in 30 minutes to avoid copyright issues. Please forward and save them.",
+            f"Files will not be deleted in 30 minutes to avoid copyright issues.",
+            f"Please forward and save them.",
             disable_web_page_preview=True,
             quote=True
         )
@@ -21,13 +25,13 @@ async def reply_forward(message: Message, file_id: int):
 async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
         if Config.FORWARD_AS_COPY is True:
-            sent_message = await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL, message_id=file_id)
+            return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL, message_id=file_id)
         elif Config.FORWARD_AS_COPY is False:
-            sent_message = await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL, message_ids=file_id)
-        return sent_message
+            return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL, message_ids=file_id)
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return media_forward(bot, user_id, file_id)
+        await message.delete()
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
     sent_message = await media_forward(bot, user_id, file_id)
@@ -37,4 +41,3 @@ async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
 async def delete_after_delay(message, delay):
     await asyncio.sleep(delay)
     await message.delete()
-
