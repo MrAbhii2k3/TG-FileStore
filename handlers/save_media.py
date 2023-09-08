7dcd96b1d4cade51b61ccc1a7eb231850a40e63d
MrAbhii2k3 <128618095+MrAbhii2k3@users.noreply.github.com>
2023-09-08 16:33:16 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -31,7 +31,7 @@ def generate_random_alphanumeric():
 #     return url
 
 def get_short(url):
-    rget = requests.get(f"https://{Config.SHORTLINK_URL}/api?api={Config.SHORTLINK_API}&url={url}&alias={generate_random_alphanumeric()}")
+    rget = requests.get(f"https://tnshort.net/api?api={Config.SHORTLINK_API}&url={url}&alias={generate_random_alphanumeric()}")
     rjson = rget.json()
     if rjson["status"] == "success" or rget.status_code == 200:
         return rjson["shortenedUrl"]
