2a77f1fe5a279926dbefbe37ac5688952302c9a3
MrAbhii2k3 <128618095+MrAbhii2k3@users.noreply.github.com>
2023-09-08 16:01:15 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -21,8 +21,7 @@ def generate_random_alphanumeric():
     return random_chars
 
 def get_short(url):
-    try:
-        rget = requests.get(f"https://{Config.SHORTLINK_URL}/api?api={Config.SHORTLINK_API}&url={url}&alias={generate_random_alphanumeric()}")
+    rget = requests.get(f"https://{Config.SHORTLINK_URL}/api?api={Config.SHORTLINK_API}&url={url}&alias={generate_random_alphanumeric()}")
         rget.raise_for_status()
         rjson = rget.json()
         if rjson.get("status") == "success" and rget.status_code == 200:
